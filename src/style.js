1d1273787833554fdd8cba799c032fd240c9306a
ryrychu <oliveros.ryanjohn@gmail.com>
2023-03-27 01:24:25 +0800

    Padding

--- src/style.js
+++ src/style.js
@@ -7,7 +7,7 @@ const styles = {
     flexCenter: "flex justify-center items-center",
     flexStart: "flex justify-center items-start",
   
-    paddingX: "sm:px-4 px-4",
+    paddingX: "sm:px-24 px-4",
     paddingY: "sm:py-16 py-6",
     paddingNavX: "lg:px-28 px-4",
     padding: "sm:px-16 px-6 sm:py-12 py-4",
